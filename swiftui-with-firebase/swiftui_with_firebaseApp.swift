94b18cdf42a61bc2007555f3718032a8d819d7f4
Sirui Li <slray@nyu.edu>
2023-07-09 16:01:44 +0800

    Initial Commit

---
+++ swiftui-with-firebase/swiftui_with_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_with_firebaseApp.swift
+//  swiftui-with-firebase
+//
+//  Created by Ray on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_with_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
